d5c5b7d56ed1c112e6084ed7fab57dac5826d609
Thesara Subasinghe <88161607+thesarakasun@users.noreply.github.com>
2022-10-15 21:04:27 +0530

    Add files via upload

---
+++ Factorial of a digit.py
@@ -0,0 +1,9 @@
+a=1
+c=int(input("Enter the number which you want to calculate the factorial = "))
+for i in range(1,c+1):
+    a=a*i
+print("\n Factorial of " + str(c) + " is =",a)
+print("\n\n\nThank you for using this calculator")
+print("thesaraks.com")
+input("\n*Press ENTER to exit")
+
